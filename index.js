6ca7effa4542e3328acd2803ac5b249f4af0c6f8
soza199 <skyysg20@gmail.com>
2026-09-24 02:05:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ client.on("ready", async () => {
   }
 
   const r = new Discord.RichPresence(client)
-    .setApplicationId(APPLICATION_ID)
+    .setApplicationId("1015128735416061952")
     .setType("PLAYING")
     .setName("INCHEON CORNER")
     .setAssetsLargeImage("https://cdn.discordapp.com/attachments/1381140647322583195/1552388329620897822/download.webp?ex=6ab56dcb&is=6ab41c4b&hm=a88f8592b26b0648f3b61e6b66e383e352f91c3cc72ec7d11c6a18c10e766e2a&")
-    .addButton("Join Server", SERVER_INVITE_URL);
+    .addButton("Join Server", "https://discord.gg/CvbBtrpCc4");
           
   client.user.setActivity(r);
 });
